0fdb9e77e39abff317b67d23f47cfe52146c4faa
Ben Rush <ben@ben-rush.net>
2024-07-22 14:56:07 -0500

    Recommendation.

--- iOS/iosMauiSwiftLibrary/SwiftLibrary.swift
+++ iOS/iosMauiSwiftLibrary/SwiftLibrary.swift
@@ -12,10 +12,10 @@ import UIKit
 public class SwiftLibrary : NSObject{
     
     @objc
-    public func returnString(input: String)->String{
+    public func returnString(input: NSString)->NSString{
         
         // returning a String breaks
-        return "You gave me " + input;
+        return ("You gave me " + (input as String)) as NSString;
         
         // if you change to return an Int, it works.
         //return 55;
